d051ca4193f1f855368c98e4fb58ad5edf89ada8
Kurt <dj3mu@darc.de>
2022-09-21 19:53:59 +0200

    Add some MTik devices

--- SnmpAbstractionTests/SnmpQuerierTests.cs
+++ SnmpAbstractionTests/SnmpQuerierTests.cs
@@ -185,7 +185,7 @@ namespace SnmpAbstractionTests
         [Test]
         public void AllForSingleIpTest()
         {
-            var ipUnderTest = new IpAddress("44.143.175.253");
+            var ipUnderTest = new IpAddress("44.148.22.34");
             var snmpVersion = SnmpVersion.Ver1;
             var useCache = false;
 
