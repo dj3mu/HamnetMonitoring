eaad7b3be99ee0d65c43b057c8c2ae85ef70d056
Kurt <dj3mu@darc.de>
2024-04-20 07:21:50 +0200

    paket update and add NanoStation M3 (untested)

--- SnmpAbstractionTests/SnmpQuerierTests.cs
+++ SnmpAbstractionTests/SnmpQuerierTests.cs
@@ -185,7 +185,7 @@ namespace SnmpAbstractionTests
         [Test]
         public void AllForSingleIpTest()
         {
-            var ipUnderTest = new IpAddress("44.148.64.141");
+            var ipUnderTest = new IpAddress("44.148.4.34");
             var snmpVersion = SnmpVersion.Ver1;
             var useCache = false;
 
