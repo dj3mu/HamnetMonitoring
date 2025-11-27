cb42a4a8f605f21b06ef67b7c4d84579ad3780f2
Kurt <dj3mu@darc.de>
2025-11-27 20:21:39 +0100

    Add Metal G-52SHPacn

--- SnmpAbstractionTests/SnmpQuerierTests.cs
+++ SnmpAbstractionTests/SnmpQuerierTests.cs
@@ -186,7 +186,7 @@ namespace SnmpAbstractionTests
         [Test]
         public void AllForSingleIpTest()
         {
-            var ipUnderTest = new IpAddress("44.148.50.118");
+            var ipUnderTest = new IpAddress("44.148.50.134");
             var snmpVersion = SnmpVersion.Ver1;
             var useCache = false;
 
@@ -206,8 +206,8 @@ namespace SnmpAbstractionTests
         [Test]
         public void IpLinkTestTest()
         {
-            var ip1UnderTest = new IpAddress("44.148.50.114");
-            var ip2UnderTest = new IpAddress("44.148.50.118");
+            var ip1UnderTest = new IpAddress("44.148.50.130");
+            var ip2UnderTest = new IpAddress("44.148.50.134");
             // var ip1UnderTest = new IpAddress("44.148.10.74");
             // var ip2UnderTest = new IpAddress("44.148.10.77");
             var snmpVersion = SnmpVersion.Ver1;
